bd7e4181d217cead7442032d5fd8be2edd49467d
Sean <74019460+hawaiiankid@users.noreply.github.com>
2021-03-16 11:10:23 -0700

    Add files via upload

---
+++ L2_WebProject_11/script.js
@@ -0,0 +1,24 @@
+let highSchoolGrade = 11;
+let yourGrade;
+switch(highSchoolGrade) {
+  case 9:
+    yourGrade = "Freshman";
+    break;
+  case 10:
+    yourGrade = "Sophomore";
+    break;
+  case 11:
+    yourGrade = "Junior";
+    break;
+  case 12:
+    yourGrade = "Senior";
+    break;
+  default:
+    yourGrade = "Invalid";
+}
+function name() {
+  let firstName = (firstName ==="John") ? document.getElementById("demo").innerHTML = "Hello John!" :  document.getElementById("demo").innerHTML = "Hello Human!";
+  
+}
+console.log(yourGrade);
+console.log(name);
